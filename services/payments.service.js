45de97c40365e9d12d442a1de27cc651313fad49
Mateusz <chomamateusz@gmail.com>
2020-01-23 08:54:25 +0100

    Added payment settings via request params nad moved seller data to .env

--- services/payments.service.js
+++ services/payments.service.js
@@ -10,7 +10,9 @@ const API_URL = getConfigOrFail('API_URL')
 const PAYMENT_RETURN_URL_SUCCESS = getConfigOrFail('PAYMENT_RETURN_URL_SUCCESS')
 const PAYMENT_RETURN_URL_FAILURE = getConfigOrFail('PAYMENT_RETURN_URL_FAILURE')
 const PAYMENT_TITLE_DEFAULT = getConfigOrFail('PAYMENT_TITLE_DEFAULT')
-const PAYMENT_SELER_NAME_DEFAULT = getConfigOrFail('PAYMENT_SELER_NAME_DEFAULT')
+const PAYMENT_SELLER_NAME_DEFAULT = getConfigOrFail('PAYMENT_SELLER_NAME_DEFAULT')
+const PAYMENT_TPAY_SELLER_ID = getConfigOrFail('PAYMENT_TPAY_SELLER_ID')
+const PAYMENT_TPAY_SECURITY_CODE = getConfigOrFail('PAYMENT_TPAY_SECURITY_CODE')
 
 module.exports = {
   name: 'payments',
@@ -26,7 +28,7 @@ module.exports = {
     returnURLSuccess: PAYMENT_RETURN_URL_SUCCESS,
     returnURLFailure: PAYMENT_RETURN_URL_FAILURE,
     responseURL: API_URL + '/api/payments/verify',
-    sellerName: PAYMENT_SELER_NAME_DEFAULT,
+    sellerName: PAYMENT_SELLER_NAME_DEFAULT,
     defaultMethod: 'tpay',
   },
 
@@ -35,24 +37,27 @@ module.exports = {
       const {
         orderId,
         method = this.settings.defaultMethod,
-        customer
+        customer,
+        settings,
       } = ctx.params
 
       if (!orderId) throw Error('You must specify orderId!')
+
+      const mergedSettings = { ...this.settings, ...settings }
 
       const { orderTotal: amount } = await this.broker.call('orders.get', { id: orderId })
 
       if (amount === 0) {
         await this.makeOrderPaid(orderId)
 
-        return { redirectURL: this.settings.returnURLSuccess }
+        return { redirectURL: mergedSettings.returnURLSuccess }
       }
 
       switch (method) {
         case 'tpay':
-          return this.createPaymentByTpay(amount, orderId, customer)
+          return this.createPaymentByTpay(amount, orderId, mergedSettings, customer)
         default:
-          return { redirectURL: this.settings.returnURLFailure }
+          return { redirectURL: mergedSettings.returnURLFailure }
       }
 
     },
@@ -70,19 +75,19 @@ module.exports = {
       return this.broker.call('orders.update', { id: orderId, status: 'paid' })
     },
     calculateResponseMd5SumTpay(transactionId, amount, crc) {
-      const sellerId = 11092
-      const securityCode = 'd07308cf4902a445'
+      const sellerId = PAYMENT_TPAY_SELLER_ID
+      const securityCode = PAYMENT_TPAY_SECURITY_CODE
 
       return crypto.createHash('md5').update(`${sellerId}${transactionId}${amount}${crc}${securityCode}`).digest('hex')
     },
     calculateRequestMd5SumTpay(amount, crc) {
-      const sellerId = 11092
-      const securityCode = 'd07308cf4902a445'
+      const sellerId = PAYMENT_TPAY_SELLER_ID
+      const securityCode = PAYMENT_TPAY_SECURITY_CODE
 
       return crypto.createHash('md5').update(`${sellerId}${amount}${crc}${securityCode}`).digest('hex')
     },
-    createPaymentByTpay(amount, orderId, customer = {}) {
-      const sellerId = 11092
+    createPaymentByTpay(amount, orderId, settings, customer = {}) {
+      const sellerId = PAYMENT_TPAY_SELLER_ID
       const url = 'https://secure.tpay.com'
       const crc = orderId
       const amountFixed = Number(amount).toFixed(2)
@@ -91,13 +96,13 @@ module.exports = {
       const data = {
         id: sellerId,
         amount: amountFixed,
-        description: this.settings.paymentTittle,
+        description: settings.paymentTittle,
         crc: orderId,
         md5sum: md5,
-        result_url: this.settings.responseURL,
-        merchant_description: this.settings.sellerName,
-        return_url: this.settings.returnURLSuccess,
-        return_error_url: this.settings.returnURLFailure,
+        result_url: settings.responseURL,
+        merchant_description: settings.sellerName,
+        return_url: settings.returnURLSuccess,
+        return_error_url: settings.returnURLFailure,
         name: customer.name || (customer.firstName + customer.lastName) || '',
         email: customer.email || '',
       }
