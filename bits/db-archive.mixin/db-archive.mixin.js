amazingdesign/amazingcms

commit d27eeed6855b99657473b2553c928a29426e9bba

Orders basket populating

mixins/collection.mixin.js
@@ -78,7 +78,6 @@ const makeCollectionService = collectionData => {
           ctxParams: JSON.parse(JSON.stringify(ctx.params)),
         })
 
-        return script.runInContext(context)
       },
     }
   }

moleculer.config.js
@@ -27,7 +27,7 @@ module.exports = {
   nodeID: null,
 
   // Enable/disable logging or use custom logger. More info: https://moleculer.services/docs/0.13/logging.html
-  logger: logger,
+  // logger: logger,
   // Log level for built-in console logger. Available values: trace, debug, info, warn, error, fatal
   logLevel: 'info',
   // Log formatter for built-in console logger. Available values: default, simple, short. It can be also a `Function`.

services/orders.service.js
@@ -6,6 +6,8 @@ const DbUtilsMixin = require('../bits/db-utilsmixin')
 const EventDispatcherMixin = require('../bits/event-dispatcher.mixin')
 const DbArchiveMixin = require('../bits/db-archive.mixin')
 
+const PRODUCT_FIELDS = ['price', 'currency', 'name', 'photo', 'published', 'description']
+
 module.exports = {
   name: 'orders',
 
@@ -76,7 +78,29 @@ module.exports = {
           type: 'object',
         }
       }
-    }
+    },
+    populates: {
+      basket: function (ids, items, rule, ctx) {
+        const populationItemsPromises = ids.map(({ id, collectionName, quantity }) => {
+          return this.broker.call(
+            'actions.get',
+            { id, fields: PRODUCT_FIELDS },
+            { meta: { collectionName } }
+          )
+            .then((product) => ({ ...product, id, collectionName, quantity }))
+            .catch(() => ({ id, collectionName, quantity }))
+        })
+
+        return Promise.all(populationItemsPromises)
+          .then((populationItems) => {
+            items.forEach((item) => {
+              item.basket = item.basket.map((basketItem) => (
+                populationItems.find((populationItem) => populationItem.id === basketItem.id)
+              ))
+            })
+          })
+      },
+    },
   },
 
   actions: {

test/orders.test.js
@@ -4,6 +4,7 @@ const { ServiceBroker } = require('moleculer')
 const Validator = require('moleculer-json-schema-validator')
 
 const OrdersService = require('../services/orders.service')
+const ActionsService = require('../services/actions.service')
 
 describe('Test "orders" service', () => {
   const mockProducts = {
@@ -44,6 +45,7 @@ describe('Test "orders" service', () => {
     }
   }
   const broker = new ServiceBroker({ logger: false, validator: new Validator() })
+  broker.createService(ActionsService)
   broker.createService(OrdersService)
   broker.createService(MockProductsService)
   broker.createService(MockBooksService)
@@ -82,7 +84,7 @@ describe('Test "orders" service', () => {
     return broker.call('orders.create', { basket })
       .then(order => expect(order.orderTotal).toBe(0))
   })
-  
+
   it('can calculate orderTotal for 1-item basket with quantity', () => {
     expect.assertions(1)
 
@@ -116,17 +118,53 @@ describe('Test "orders" service', () => {
     return broker.call('orders.create', { basket })
       .then(order => expect(order.orderTotal).toBe(0))
   })
- 
+
   it('can calculate orderTotal for 2-items basket with different collections products', () => {
     expect.assertions(1)
 
     const basket = [
-      { id: '5dd65b0a0d02f941837773aa', collectionName: 'products'},
+      { id: '5dd65b0a0d02f941837773aa', collectionName: 'products' },
       { id: '5d7e6a5542dee364294cff2c', collectionName: 'books', quantity: 1 },
     ]
 
     return broker.call('orders.create', { basket })
       .then(order => expect(order.orderTotal).toBe(170))
+  })
+
+  it('can populate products in basket', () => {
+    expect.assertions(1)
+
+    const basket = [
+      { id: '5dd65b0a0d02f941837773aa', collectionName: 'products' },
+      { id: '5dd679590d02f941837773ac', collectionName: 'products' },
+    ]
+
+    return broker.call('orders.create', { basket })
+      .then(({ _id }) =>  broker.call('orders.get', { id: _id, populate: ['basket'] }))
+      .then((order) => (
+        expect(order.basket).toEqual([
+          { ...basket[0], name: 'Mouse', price: 150 },
+          { ...basket[1], name: 'Phone', price: 1000  },
+        ])
+      ))
+  })
+
+  it('can populate products in basket for multiple collections', () => {
+    expect.assertions(1)
+
+    const basket = [
+      { id: '5dd65b0a0d02f941837773aa', collectionName: 'products' },
+      { id: '5d7e6a5542dee364294cff2c', collectionName: 'books', quantity: 1 },
+    ]
+
+    return broker.call('orders.create', { basket })
+      .then(({ _id }) =>  broker.call('orders.get', { id: _id, populate: ['basket'] }))
+      .then((order) => (
+        expect(order.basket).toEqual([
+          { ...basket[0], name: 'Mouse', price: 150 },
+          { ...basket[1], name: 'Harry Potter', price: 20 },
+        ])
+      ))
   })
 
 })

commit c7a1ebca9e934fca1714e2c279ede26d86ad18a7

Fixed collection afterHook and added ctx.meta to context
Fixed error caused in d27eeed6855b99657473b2553c928a29426e9bba 1.9.0 - deleting line by mistake

bits/db-archive.mixin/db-archive.mixin.js
@@ -76,7 +76,7 @@ module.exports = {
         // @HACK must check calledByApi because before remove call
         // is get call (probably by hook in moleculer-db) and it will
         // fail here when calling remove to unarchive already archived item
-        if (item._archived && ctx.meta.calledByApi) {
+        if (item && item._archived && ctx.meta.calledByApi) {
           throw new WebErrors.NotFoundError()
         }
       }

mixins/collection.mixin.js
@@ -76,8 +76,10 @@ const makeCollectionService = collectionData => {
           actionName: ctx.action.rawName,
           serviceName: ctx.action.name.split('.')[0],
           ctxParams: JSON.parse(JSON.stringify(ctx.params)),
+          ctxMeta: JSON.parse(JSON.stringify(ctx.meta)),
         })
-
+        
+        return script.runInContext(context)
       },
     }
   }